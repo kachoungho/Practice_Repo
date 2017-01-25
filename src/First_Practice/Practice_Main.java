21b0d4d04c5321abffbc76e0022aa8efe23431c5
kachoungho <kachounghoa@naver.com>
2017-01-25 11:26:21 +0900

    TIS-2 TIS-3 여러 이슈 등록 테스트

--- src/First_Practice/Practice_Main.java
+++ src/First_Practice/Practice_Main.java
@@ -3,5 +3,8 @@ package First_Practice;
 public class Practice_Main {
 	public static void main(String[] args) {
 		System.out.println("test");
+		System.out.println("test2");
+		System.out.println("test3");
+		System.out.println("test4");
 	}
 }
